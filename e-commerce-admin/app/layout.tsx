d00dc524e6b5aa9451fc58c55ea25945636e0779
Amilcar Steban Rodriguez <71573575+Amilcar-Steban@users.noreply.github.com>
2024-06-07 20:52:45 -0500

    Update layout.tsx

--- e-commerce-admin/app/layout.tsx
+++ e-commerce-admin/app/layout.tsx
@@ -10,7 +10,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Amilcar-Camilo-Sebas',
+  title: 'Hola mundo',
   description: 'E-Commerce Dashboard',
 }
 
